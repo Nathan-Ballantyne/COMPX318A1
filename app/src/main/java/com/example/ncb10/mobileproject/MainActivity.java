a83cc2e6cd3e1ed4014b9ab152cf377fe79bb2bc
WooBilly <billywoo00@gmail.com>
2019-07-24 11:55:43 +1200

    Added in Gym location text and edittextbox

--- app/src/main/java/com/example/ncb10/mobileproject/MainActivity.java
+++ app/src/main/java/com/example/ncb10/mobileproject/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.ncb10.mobileproject;
 import android.app.Dialog;
 import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.graphics.Color;
 import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
@@ -12,12 +13,15 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.text.Layout;
 import android.view.Gravity;
+import android.view.KeyEvent;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.ViewGroup;
+import android.view.inputmethod.InputMethodManager;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.LinearLayout;
 import android.widget.PopupWindow;
 import android.widget.TextView;
@@ -67,7 +71,9 @@ public class MainActivity extends AppCompatActivity {
         PopupWindow popUp;
         LinearLayout layout;
         TextView tv;
+        TextView tv2;
         ViewGroup.LayoutParams params;
+        final EditText editText;
         LinearLayout mainLayout;
         Button but;
         boolean click = true;
@@ -76,6 +82,21 @@ public class MainActivity extends AppCompatActivity {
         layout = new LinearLayout(this);
         //mainLayout = new LinearLayout(this);
         tv = new TextView(this);
+        tv2 = new TextView(this);
+        editText = new EditText(this);
+        editText.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+
+
+
+
+        InputMethodManager im = (InputMethodManager) getSystemService(Context.INPUT_METHOD_SERVICE);
+        im.showSoftInput(editText, InputMethodManager.SHOW_FORCED);
+
         if (click) {
             //popUp.showAtLocation(layout, Gravity.TOP, -300, 240);
             popUp.update(-0, 0, 900, 1350);
@@ -88,7 +109,10 @@ public class MainActivity extends AppCompatActivity {
                 ViewGroup.LayoutParams.WRAP_CONTENT);
         layout.setOrientation(LinearLayout.VERTICAL);
         tv.setText("Hi this is a sample text for popup window");
+        tv2.setText("Gym Location/Name : ");
         layout.addView(tv, params);
+        layout.addView(tv2, params);
+        layout.addView(editText, params);
         popUp.setContentView(layout);
         popUp.showAtLocation(layout, Gravity.CENTER, 0, 0);
         //-300,240
